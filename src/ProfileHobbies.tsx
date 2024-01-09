c91cac4407e8a02d11d5afc0e429214f0662be01
Andrey Belomyttsev <andymedtech@gmail.com>
2024-01-09 13:30:53 +0300

    init: add homework 02

---
+++ src/ProfileHobbies.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ProfileHobbies = () => {
+  return (
+    <div>
+      <h3>Увлечения:</h3>
+      <ul>
+        <li>Чтение</li>
+        <li>Плавание</li>
+        <li>Искусство</li>
+      </ul>
+    </div>
+  );
+};
